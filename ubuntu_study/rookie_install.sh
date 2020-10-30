b2d249b8ad06607fe535562986ba664ff85a99a4
xueqing <haijuanchen.sun@gmail.com>
2020-10-30 09:55:24 +0800

    Update rookie_install.sh

--- ubuntu_study/rookie_install.sh
+++ ubuntu_study/rookie_install.sh
@@ -20,6 +20,7 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.ss 'stash save'
+git config --global alias.sl 'stash list'
 git config --global alias.sp 'stash pop'
 git config --global alias.unstage 'reset HEAD --'
 
